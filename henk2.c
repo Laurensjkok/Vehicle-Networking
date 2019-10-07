7860271c4b2eaeddb8c3c1621bf9e947e42cc0bd
Kok <l.j.kok@student.tue.nl>
2019-10-07 20:13:59 +0200

    Combined versions
    merged henk3 into henk2.

--- henk2.c
+++ henk2.c
@@ -195,16 +195,66 @@ void make_frame()
 
 
 
-int main()
+
+static void hw_can_mac_driver(
+		       volatile CAN_PORT *can_port_id,
+		       CAN_FRAME * volatile *TxFrameFromSensor,
+		       CAN_FRAME * volatile *RxFrameForActuator,
+		       int *rxPrioFilters, uint rxPrioFiltersLen)
 {
-	make_frame();
-	printf("\n");
-	for (int j = 0; j < (stuffedlength + 13); ++j)
-	{
-		printf("%d", frame[j]);
-	}	
-	
-	
+  CAN_FRAME TxFrame, RxFrame;
+  bool newFrameFromSensor;
+  CAN_SYMBOL TxSymbol, RxSymbol;
+ 
+  
+ while (1) {
+    /* if (*rxPrioFilters) < 0 then we're master else slave
+       as a master, to get the next frame to send from the sensor use:
+       newFrameFromSensor = can_mac_rx_next_frame(TxFrameFromSensor, &TxFrame);
+       as a slave, to send a received frame with priority rxPrioFilter to the actuator use:
+       can_mac_tx_next_frame(RxFrameForActuator, &RxFrame);
+    */
+    
+    /* to send a CAN symbol on the CAN bus use:
+       can_phy_tx_symbol(can_port_id, TxSymbol)
+       to receive a CAN symbol from the CAN bus use:
+       can_phy_rx_symbol_blocking(can_port_id,&RxSymbol)
+       this function blocks until a new symbol is available on the bus
+    */
+    make_frame();
+    
+   for (int j = 0; j<stuffedlength+13; j++){
+     if (frame[j] == 0){
+     can_phy_tx_symbol(can_port_id, DOMINANT);
+     can_phy_rx_symbol_blocking(can_port_id,&RxSymbol);
+     }
+     else
+     {
+         can_phy_tx_symbol(can_port_id, RECESSIVE);
+        can_phy_rx_symbol_blocking(can_port_id,&RxSymbol);
+     }
+     
+   }
+    newFrameFromSensor = can_mac_rx_next_frame(TxFrameFromSensor, &TxFrame);
+    if (newFrameFromSensor == true) {
+      /* if you wish to send different test sequences from different sensors,
+       * then this is how you can do this. Note that this should not be used in the final driver,
+       * since there all sensors must be treated the same.
+       */
+      if (TxFrame.ID == 0) {
+        /* sensor 1 with priority/ID 0 */
+        can_phy_tx_symbol(can_port_id, DOMINANT);
+        can_phy_rx_symbol_blocking(can_port_id,&RxSymbol);
+        can_phy_tx_symbol(can_port_id, DOMINANT);
+        can_phy_rx_symbol_blocking(can_port_id,&RxSymbol);
+        can_phy_tx_symbol(can_port_id, RECESSIVE);
+        can_phy_rx_symbol_blocking(can_port_id,&RxSymbol);
+      }
+      else { 
+        /* the other active sensors */
+        can_phy_tx_symbol(can_port_id, RECESSIVE);
+        can_phy_rx_symbol_blocking(can_port_id,&RxSymbol);
+      }
+    }
+  }
 }
-
-
