af5b042bea85f543228a7bde9dba8d0bf0010559
Laurens Kok <l.j.kok@student.tue.nl>
2019-10-09 09:35:37 +0200

    <Message>

---
+++ Version4/can_mac/sensor1_config.h
@@ -0,0 +1,6 @@
+#define CAN_MSG_PRIO_1                            0
+#define CAN_MSG_TX_PERIOD_1                       6750000000
+#define CAN_MSG_TX_OFFSET_1                       0
+#define CAN_MSG_TX_DLC_1                          3
+
+
