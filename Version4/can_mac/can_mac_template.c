bdeaf950181018f5957150f5b2e6c08a217d1c8d
Laurensjkok <56067684+Laurensjkok@users.noreply.github.com>
2019-10-14 11:30:28 +0200

    Update can_mac_template.c

--- Version4/can_mac/can_mac_template.c
+++ Version4/can_mac/can_mac_template.c
@@ -292,7 +292,7 @@ if ((*rxPrioFilters) < 0){ //then we're master else slave
 }
 
 else{// you are actuator
-mk_mon_debug_info(0xZ);
+mk_mon_debug_info(0x1234);
 int EOFCounter = 0, ErrorCounter = 0, stuffedBit=0;
 	while(1){ 
 		//restart listening
