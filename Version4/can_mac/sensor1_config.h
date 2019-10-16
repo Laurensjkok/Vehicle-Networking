877e58f32f6feee33a9978ff45b67ecde073ff41
Laurensjkok <56067684+Laurensjkok@users.noreply.github.com>
2019-10-16 16:23:55 +0200

    Update sensor1_config.h

--- Version4/can_mac/sensor1_config.h
+++ Version4/can_mac/sensor1_config.h
@@ -1,6 +1,6 @@
 #define CAN_MSG_PRIO_1                            0
 #define CAN_MSG_TX_PERIOD_1                       220000000
 #define CAN_MSG_TX_OFFSET_1                       0
-#define CAN_MSG_TX_DLC_1                          1
+#define CAN_MSG_TX_DLC_1                          3
 
 
