0d90631b090f2825ebae40d7da4ec611ceae5fa2
SMolenschot <stefanm@thor.edu>
2019-10-16 18:38:33 +0200

    test3

--- Version4/can_mac/sensor1_config.h
+++ Version4/can_mac/sensor1_config.h
@@ -1,5 +1,5 @@
 #define CAN_MSG_PRIO_1                            0
-#define CAN_MSG_TX_PERIOD_1                       220000000
+#define CAN_MSG_TX_PERIOD_1                       1040000000
 #define CAN_MSG_TX_OFFSET_1                       0
 #define CAN_MSG_TX_DLC_1                          1
 
