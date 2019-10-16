c60e6be08a2be9cff8dbbf3eafa9f10932546057
Laurensjkok <56067684+Laurensjkok@users.noreply.github.com>
2019-10-16 11:59:12 +0200

    Update actuator2_config.h

--- Version4/can_mac/actuator2_config.h
+++ Version4/can_mac/actuator2_config.h
@@ -1,8 +1,8 @@
 #define CAN_MSG_RX_PERIOD_2      (6750000000)  //137 symbols * 2 = 2 * Worst_Case_CAN_frame
 #define CAN_MSG_RX_OFFSET_2      (3500000000)  //nr of tile clk cycles
 
-#define CAN_MSG_RX_PRIOS_2   4
-int rxPrioFilt_2[CAN_MSG_RX_PRIOS_2] = {0,1,2,3};   //message ID filters
+// #define CAN_MSG_RX_PRIOS_2   4
+// int rxPrioFilt_2[CAN_MSG_RX_PRIOS_2] = {0,1,2,3};   //message ID filters
 
-//#define CAN_MSG_RX_NUM_FILTERS_2   1
-//int rxPrioFilt_2[CAN_MSG_RX_NUM_FILTERS_2] = {-1};   //message ID filters
+#define CAN_MSG_RX_NUM_FILTERS_2   1
+int rxPrioFilt_2[CAN_MSG_RX_NUM_FILTERS_2] = {-1};   //message ID filters
