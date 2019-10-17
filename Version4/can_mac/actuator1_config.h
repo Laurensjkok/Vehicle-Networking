b011bb00ee2c4b2166d8a3423d73055eeffc2ad0
SMolenschot <stefanm@thor.edu>
2019-10-17 08:59:10 +0200

    heeeeennnk

--- Version4/can_mac/actuator1_config.h
+++ Version4/can_mac/actuator1_config.h
@@ -1,8 +1,8 @@
 #define CAN_MSG_RX_PERIOD_1      (6750000000)  //137 symbols * 2 = 2 * Worst_Case_CAN_frame
 #define CAN_MSG_RX_OFFSET_1      (3500000000)  //nr of tile clk cycles
 
-#define CAN_MSG_RX_PRIOS_1                   3
-int rxPrioFilt_1[CAN_MSG_RX_PRIOS_1] = {1,2,3};  //message ID filters
+#define CAN_MSG_RX_PRIOS_1                   2
+int rxPrioFilt_1[CAN_MSG_RX_PRIOS_1] = {2,3};  //message ID filters
 
 //#define CAN_MSG_RX_NUM_FILTERS_1                    1
 //int rxPrioFilt_1[CAN_MSG_RX_NUM_FILTERS_1] = {-1};  //message ID filters
